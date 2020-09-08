fe3305d9fe11c0e221e06cf97a619fa8864bd5fd
Jesse <2302541+Kangaroux@users.noreply.github.com>
2020-09-08 14:03:15 -0400

    Fix 404 error when navigating to /admin from the frontend

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,8 +1,28 @@
-import { Route, Router } from "preact-router";
+import { getCurrentUrl, Route, Router } from "preact-router";
 import { h } from "preact";
 
 import { Home } from "./components/views/Home";
 import { Error } from "./components/views/Error";
+
+
+const externalURL = new RegExp(`^/(admin|api|static)/`);
+
+
+/**
+ * The route used if no other routes matched.
+ */
+const DefaultRoute = () => {
+    const url = getCurrentUrl();
+
+    // Check if the URL should be handled by the SPA. If it's not handled, redirect to
+    // the page, otherwise show a 404 error.
+    if (externalURL.test(url)) {
+        window.location.replace(url);
+        return <div></div>;
+    }
+
+    return <Error code={404} />
+}
 
 
 export const Routes = () => {
@@ -15,7 +35,7 @@ export const Routes = () => {
         <Router onChange={handleRoute}>
             <Route path="/" component={Home} />
 
-            <Route component={() => <Error code={404} />} default />
+            <Route default component={DefaultRoute} />
         </Router>
     );
 }
